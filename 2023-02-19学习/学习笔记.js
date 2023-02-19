f3b48dda3829f81e95e14632bd03defaf60bae5b
ZHANGJINXIN <feideyong1@gmail.com>
2023-02-19 17:39:47 +0800

    html web vscode html标签学习

---
+++ 2023-02-19学习/学习笔记.js
@@ -0,0 +1,179 @@
+/*
+!重要
+TODO警告
+*提示
+?代码块
+                                                    一、HTML学习笔记
+    !(一)、网页为什么叫HTML 
+        *1、网页需要通过浏览器才能访问阅读
+        *2、网页是网站中的“一页”
+        *3、文件扩展名.HTML(一套标记语言)
+            eg:<center></center>
+        *4、浏览器 兼容性查询： can I use
+        
+    !(二)、WEB(标准的制定 伯纳斯·李  W3C--万维网联盟  统一浏览器兼容性标准 EDge6 不兼容)
+        *1、Web网页的组成部分【 HTML（框架）、CSS(打扮）、JS（交互行为）】
+            TODO【文档声明标签】 此文档按照最新版html解析本页面
+            ?<!DOCTYPE html>
+            TODOlang 编写代码使用的语言  en 英语 zh-CN中文 ja-gp 日文 
+            ?<html lang="en">
+                ?<head>
+                    TODOmeta是标签 charset是字符集 此代码用"UTF-8"编码 浏览器使用"UTF-8"解码（万国码字符编码）
+                    ?<meta charset="UTF-8">
+                    ?<meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    ?<meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    ?<title>使用VSCODE 创建的第一个网页</title>
+                ?</head>
+                ?<body>
+                    ?<center>hello world</center>
+                ?</body>
+            ?</html>
+        
+      !(三)、HTML常用标签
+      *文本标题
+      ?<h1>一级标题 </h1>
+      ?<h1>二级标题 </h1>
+      ?<h1>三级标题 </h1>
+      ?<h1>四级标题 </h1>
+      ?<h1>五级标题 </h1>
+      ?<h1>六级标题 </h1>
+      *段落文本
+      ?<p>段落文本内容</P>
+      *水平线 单标签
+      ?<hr/>
+      *加粗（推荐strong)
+      ?<b>加粗内容</b> 只是显示加粗
+      ?<strong>强调的内容</strong>突出的文本
+      *倾斜(推荐em)
+      ?<em>强调文本</em>
+      ?<i>倾斜文本</i>
+      *删除线(推荐del)
+       ?<s>文本</s>删除线
+       ?<del>文本</del>删除线
+      *扩展
+      ?<u>文本</u>下划线
+      ?<sub></sub>下标
+      ?<sup></sup>上标
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
